d73c4c5f7f384c34fadbf238523aed4dc7f48120
Noodlewood <daniel.riedmuller@gmail.com>
2014-02-17 20:47:26 +0100

    html structure

--- js/App.js
+++ js/App.js
@@ -6,6 +6,18 @@ TWRP.ns('TWRP');
 TWRP.App = Class.extend(TWRP.util.Observable, {
 
 	initialize: function() {
+		this.centerContent();
+		this.initNaviButtons();
+	},
 
+	centerContent: function() {
+		var centerPadding = $(window).width() / 5;
+		$('.center').css('padding', '25px ' + centerPadding + 'px');
+	},
+
+	initNaviButtons: function() {
+		$('.navi-btn').click(function() {
+			$("html, body").animate({scrollTop: $('.' + $(this).text().toLocaleLowerCase()).position().top + "px" });
+		});
 	}
 });
